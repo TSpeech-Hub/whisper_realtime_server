13e712f137d29f67d8b89a8e4f101ba780ab443e
Dario Pellegrino <dario.pellegrino00@gmail.com>
2024-11-22 12:24:50 +0100

    support for whisper large-v3-turbo

--- src/client.py
+++ src/client.py
@@ -1,12 +1,13 @@
 from dataclasses import dataclass
+from whisper_online import *
 import sounddevice as sd
 import numpy as np
-import socket, threading, re, sys 
+import socket, threading, sys 
 
 @dataclass
 class AudioConfig:
     sample_rate = 16000
-    chunk_duration = 1.0
+    chunk_duration = 1
     channels = 1
     chunk_size = int(sample_rate * chunk_duration)
 
@@ -15,8 +16,10 @@ class NetConfig:
     host = 'localhost'
     port = 12001
 
+
 class TranscriptorClient:
  
+    mean_time, n_responses = 0, 0
     @staticmethod 
     def __error(msg):
         sys.stderr.write(f"\033[91m" + msg + "\033[0m\n")
@@ -42,12 +45,12 @@ class TranscriptorClient:
         """Receive transcriptions from the server."""
         try:
             while True:
-                response = sock.recv(1024).decode('utf-8').strip()
-                text = response.split(' ', 2)[2].strip() # NNNNN MMMMM text
-                if re.search('[.]+', text) == None: 
-                    sys.stdout.write(text + " ")
-                else: 
-                    sys.stdout.write(text + "\n")
+                response = sock.recv(1024).decode('utf-8').strip().split(' ', 2)
+                self.mean_time += abs (float(response[1]) - float(response[0]))
+                self.n_responses += 1
+
+                if response:
+                    print(f"{response}")
         except Exception as e:
             TranscriptorClient.__error(f"Error in receiving transcription: {e}")
 
@@ -68,6 +71,8 @@ class TranscriptorClient:
                     
                     while sender_thread.is_alive() and receiver_thread.is_alive(): 
                         threading.Event().wait(0.1)
+        except KeyboardInterrupt : 
+            print(f"mean time to responde {self.mean_time / self.n_responses}")
         except Exception as e:
             TranscriptorClient.__error(f"Error {e}")
 
