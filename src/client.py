9c7a2afce61e7348ebf6d3527273c50ecef95867
Dario Pellegrino <dario.pellegrino00@gmail.com>
2024-12-10 11:13:03 +0100

    audio streaming simulation with audio path

--- src/client.py
+++ src/client.py
@@ -1,9 +1,9 @@
 from dataclasses import dataclass
-from whisper_online import *
+from typing import Text
 from OpenSSL import SSL
 import sounddevice as sd
 import numpy as np
-import socket, threading, sys 
+import socket, threading, sys, time, librosa, argparse
 
 #NOTE: just to test the server
 
@@ -59,7 +59,7 @@ class TranscriptorClient:
                 print(f"Whisper server ready on port: {whisper_port}")
                 return int(whisper_port)
             else:
-                print(f"Error: {whisper_port}")
+                print(f"Error during connection invalid whisper port: {whisper_port}")
         except KeyboardInterrupt:
             print("Stopping client...")
         except SSL.Error as ssl_error:
@@ -72,51 +72,7 @@ class TranscriptorClient:
             conn.shutdown()
             conn.close()
 
-    def __send_audio(self, stream, sock):
-        """Capture audio from the microphone and send it to the server."""
-        try:
-            while True:
-                # Capture audio chunk
-                now = time.time()
-                audio_data = stream.read(AudioConfig.chunk_size)[0]
-                audio_chunk = (audio_data[:, 0] * 32768).astype(np.int16).tobytes()
-                with self.__time_lock:
-                    if self.timestamp == 0:
-                        self.timestamp = time.time() - now
-
-                
-                # Send audio chunk to server
-                sock.sendall(audio_chunk)
-        except Exception as e:
-            TranscriptorClient.__error(f"Error in sending audio: {e}")
-
-    def __receive_transcriptions(self, sock):
-        """Receive transcriptions from the server."""
-        
-        count, sum = 0, 0
-        try:
-            while True:
-                now = time.time()
-                response = sock.recv(1024).decode('utf-8')
-                splitted = response.split(" ")
-
-                if response:
-                    with self.__time_lock:
-                        timestamp = round(time.time() - now, 2)
-                        self.timestamp = 0
-                    sum += timestamp
-                    count += 1
-                    #NOTE: staying silent for a while increase the time between chunks, this is just for debugging
-                    print(f"time since last audio chunk was sent: {timestamp} seconds")
-                    print(f"{response}")
-        except Exception as e:
-            TranscriptorClient.__error(f"Error in receiving transcription: {e}")
-        finally:
-            if count == 0:
-                TranscriptorClient.__error("No transcriptions received")
-            else:
-                TranscriptorClient.__error(f"Average time: {sum/count} milliseconds")
-
+    #TODO: start and simulate are duplicated 
     def start(self, port):
         """Initialize the microphone stream and handle client-server communication."""
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -143,11 +99,122 @@ class TranscriptorClient:
         except KeyboardInterrupt : 
             TranscriptorClient.__log("Stopping client...")
         except Exception as e:
+            TranscriptorClient.__error(f"Error in start{e}")
+        finally: 
+            s.close()
+
+    def simulate(self, filename, port): 
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        conn = SSL.Connection(setup_ssl_context(), s)
+        try:
+            #NOTE: SSL connection handshake
+            conn.connect((NetConfig.host, port))
+            conn.set_connect_state()
+            conn.do_handshake()
+            TranscriptorClient.__log(f"Connected to server at {NetConfig.host}:{NetConfig.port}")
+            # Initialize microphone
+                # Connect to the server
+                
+            sender_thread = threading.Thread(target=self.__send_audio_simulation, args=(filename, conn), daemon=True)
+            receiver_thread = threading.Thread(target=self.__receive_transcriptions, args=(conn,), daemon=True)
+            
+            sender_thread.start()
+            receiver_thread.start()
+        
+            while sender_thread.is_alive() and receiver_thread.is_alive(): 
+                threading.Event().wait(0.1)
+
+        except KeyboardInterrupt : 
+            TranscriptorClient.__log("Stopping client...")
+        except Exception as e:
             TranscriptorClient.__error(f"Error {e}")
         finally: 
             s.close()
 
+
+    def __send_audio_simulation(self, filename, sock):
+        try: 
+            audio_data, file_sample_rate = librosa.load(filename, sr=16000, mono=True)
+
+            num_samples = len(audio_data) 
+
+            for start in range(0, num_samples, 16000):
+                end = start + 16000
+                chunk = audio_data[start:end]
+
+                # Scala e converti il chunk in int16
+                audio_chunk = (chunk * 32768).astype(np.int16).tobytes()
+
+                # Invia il chunk tramite la socket
+                sock.sendall(audio_chunk)
+
+                # Aspetta 1 secondo per simulare l'invio in tempo reale
+                time.sleep(1)        
+        except Exception as e:
+            TranscriptorClient.__error(f"Error in sending audio: {e}")
+
+    def __send_audio(self, stream, sock):
+        """Capture audio from the microphone and send it to the server."""
+        try:
+            while True:
+                # Capture audio chunk
+                now = time.time()
+                audio_data = stream.read(AudioConfig.chunk_size)[0]
+                audio_chunk = (audio_data[:, 0] * 32768).astype(np.int16).tobytes()
+
+                with self.__time_lock:
+                    if self.timestamp == 0:
+                        self.timestamp = time.time() - now
+
+                
+                # Send audio chunk to server
+                sock.sendall(audio_chunk)
+        except Exception as e:
+            TranscriptorClient.__error(f"Error in sending audio: {e}")
+
+    def __receive_transcriptions(self, sock):
+        """Receive transcriptions from the server."""
+        
+        print("DEBUG: receiving")
+        count, sum = 0, 0
+        try:
+            while True:
+                now = time.time()
+                response = sock.recv(1024).decode('utf-8')
+                splitted = response.split(" ")
+
+                print("DEBUG: received")
+                if response:
+                    with self.__time_lock:
+                        timestamp = round(time.time() - now, 2)
+                        self.timestamp = 0
+                    sum += timestamp
+                    count += 1
+                    #NOTE: staying silent for a while increase the time between chunks, this is just for debugging
+                    print(f"time since last audio chunk was sent: {timestamp} seconds")
+                    print(f"{response}")
+        except Exception as e:
+            TranscriptorClient.__error(f"Error in receiving transcription: {e}")
+        finally:
+            if count == 0:
+                TranscriptorClient.__error("No transcriptions received")
+            else:
+                TranscriptorClient.__error(f"Average time: {sum/count} milliseconds")
+
+
+args = argparse.ArgumentParser("optional file path for simulation")
+args.add_argument('--filename', type=Text,
+                    help='audio file path')
+
 if __name__ == "__main__":
     client = TranscriptorClient()
     port = client.connect_to_server(NetConfig.host, NetConfig.port)
-    client.start(port)
+
+    filename = args.parse_args().filename
+
+    if filename: 
+        print("DEBUG: simulation")
+        client.simulate(filename, port)
+    else: 
+        print("DEBUG: real")
+        client.start(port)
